f76932ac3137149f24140f99bfd21f92e3fd5933
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2023-03-11 11:08:45 +0100

    added the dropdown for the table description

--- src/components/users/UsersTable.tsx
+++ src/components/users/UsersTable.tsx
@@ -1,11 +1,126 @@
-import React from 'react'
+import React, { useState } from "react";
+import classes from "./UsersTable.module.scss";
+import FilterButton from "@/assets/filterbutton.svg";
+import Icmore from "@/assets/icmore.svg";
+import DeleteIcon from "@/assets/dropdownicons/delfriend.svg";
+import UserIcon from "@/assets/dropdownicons/user.svg";
+import ViewIcon from "@/assets/dropdownicons/view.svg";
 
-type Props = {}
+type Props = {};
 
 const UserTable = (props: Props) => {
-  return (
-    <div>UserTable goes here</div>
-  )
-}
+  const [toggleDropdown, setToggleDropdown] = useState<boolean>(false);
+  const [toggleFilter, setToggleFilter] = useState<boolean>(false);
 
-export default UserTable
+  return (
+    <div>
+      <div className={classes.container}>
+        <table>
+          <thead>
+            <tr>
+              <th>
+                Organization{" "}
+                <span>
+                  <img src={FilterButton} alt="filter button" />
+                </span>
+              </th>
+              <th>
+                Username{" "}
+                <span>
+                  <img src={FilterButton} alt="filter button" />
+                </span>
+              </th>
+              <th>
+                Email{" "}
+                <span>
+                  <img src={FilterButton} alt="filter button" />
+                </span>
+              </th>
+              <th>
+                Phone number{" "}
+                <span>
+                  <img src={FilterButton} alt="filter button" />
+                </span>
+              </th>
+              <th>
+                Date joined{" "}
+                <span>
+                  <img src={FilterButton} alt="filter button" />
+                </span>
+              </th>
+              <th>
+                Status{" "}
+                <span>
+                  <img src={FilterButton} alt="filter button" />
+                </span>
+              </th>
+              {/* <th></th> */}
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Lendsqr</td>
+              <td>Adedeji</td>
+              <td>adedeji@lendsqr.com</td>
+              <td>08078903721</td>
+              <td>May 15, 2020 10:00 AM</td>
+              <td>Inactive</td>
+              <td>
+                <img
+                  onClick={() => {
+                    setToggleDropdown(!toggleDropdown);
+                  }}
+                  src={Icmore}
+                  alt="more options button"
+                />
+                {toggleDropdown && (
+                  <div className={classes.dropdown}>
+                    <ul>
+                      <li>
+                        <img src={ViewIcon} alt="view icon" />
+                        <span>View Details</span>
+                      </li>
+                      <li>
+                        <img src={DeleteIcon} alt="view icon" />
+                        <span>Blacklist User</span>
+                      </li>
+                      <li>
+                        <img src={UserIcon} alt="view icon" />
+                        <span>Activate User</span>
+                      </li>
+                    </ul>
+                  </div>
+                )}
+              </td>
+            </tr>
+            <tr>
+              <td>Lendsqr</td>
+              <td>Adedeji</td>
+              <td>adedeji@lendsqr.com</td>
+              <td>08078903721</td>
+              <td>May 15, 2020 10:00 AM</td>
+              <td>Inactive</td>
+              <td>
+                <img src={Icmore} alt="more options button" />
+              </td>
+            </tr>
+            <tr>
+              <td>Lendsqr</td>
+              <td>Adedeji</td>
+              <td>adedeji@lendsqr.com</td>
+              <td>08078903721</td>
+              <td>May 15, 2020 10:00 AM</td>
+              <td>Inactive</td>
+              <td>
+                <img src={Icmore} alt="more options button" />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default UserTable;
