03d35f9b7e27ed6a56023863554730ba611b7824
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2023-03-17 20:44:03 +0100

    added scroll to top on userdetail page

---
+++ src/App.test.ts
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('App', () => {
+  it('should be defined', () => {
+    expect(App).toBeDefined();
+  });
+
+  // it('should render correctly', () => {
+  //   render(<App />);
+  // });
+});
---
+++ src/pages/Login.test.ts
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+// import Login from './Login';
+
+// describe('Login component', () => {
+//  it("should have a login button", () => {
+//    const { getByText } = render(<Login />);
+//    expect(getByText("Login")).toBeInTheDocument();
+//  })
+// })
